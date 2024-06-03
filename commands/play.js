1241e39b5a57743a79bc4a2a8b6f07cbc1e9dbcd
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-06-03 22:27:20 +0800

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,121 +1,113 @@
-
 const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 
 const queueNames = [];
 
 async function play(client, interaction) {
-    try {
-        const query = interaction.options.getString('nama'); 
+  try {
+    const query = interaction.options.getString('nama');
 
-        const player = client.riffy.createConnection({
-            guildId: interaction.guildId,
-            voiceChannel: interaction.member.voice.channelId,
-            textChannel: interaction.channelId,
-            deaf: true
-        });
+    const player = client.riffy.createConnection({
+      guildId: interaction.guildId,
+      voiceChannel: interaction.member.voice.channelId,
+      textChannel: interaction.channelId,
+      deaf: true
+    });
 
-     
-        await interaction.deferReply();
+    await interaction.deferReply();
 
-   
-        const resolve = await client.riffy.resolve({ query: query, requester: interaction.user });
-        const { loadType, tracks, playlistInfo } = resolve;
+    const resolve = await client.riffy.resolve({ query: query, requester: interaction.user });
+    const { loadType, tracks, playlistInfo } = resolve;
 
-        if (loadType === 'playlist') {
-            for (const track of resolve.tracks) {
-                track.info.requester = interaction.user;
-                player.queue.add(track);
-                queueNames.push(track.info.title); 
-            }
+    if (loadType === 'playlist') {
+      for (const track of resolve.tracks) {
+        track.info.requester = interaction.user;
+        player.queue.add(track);
+        queueNames.push(track.info.title);
+      }
 
-            if (!player.playing && !player.paused) player.play();
+      if (!player.playing && !player.paused) player.play();
 
-        } else if (loadType === 'search' || loadType === 'track') {
-            const track = tracks.shift();
-            track.info.requester = interaction.user;
+    } else if (loadType === 'search' || loadType === 'track') {
+      const track = tracks.shift();
+      track.info.requester = interaction.user;
 
-            player.queue.add(track);
-            queueNames.push(track.info.title);
+      player.queue.add(track);
+      queueNames.push(track.info.title);
 
-            if (!player.playing && !player.paused) player.play();
-        } else {
-            const errorEmbed = new EmbedBuilder()
-                .setColor('#ff0000')
-                .setTitle('Error')
-                .setDescription('Tiada lagu yang dijumpai.');
+      if (!player.playing && !player.paused) player.play();
+    } else {
+      const errorEmbed = new EmbedBuilder()
+        .setColor('#ff0000')
+        .setTitle('Error')
+        .setDescription('Tiada lagu yang dijumpai.');
 
-        
-            await interaction.editReply({ embeds: [errorEmbed] });
-            return;
-        }
+      await interaction.editReply({ embeds: [errorEmbed] });
+      return;
+    }
 
-       
-        await new Promise(resolve => setTimeout(resolve, 500));
-        
-     
-const { EmbedBuilder } = require("discord.js");
+    await new Promise(resolve => setTimeout(resolve, 500));
 
-
-const embeds = [
-  
-    new EmbedBuilder()
+    const embeds = [
+      new EmbedBuilder()
         .setColor('#4d9fd6')
         .setAuthor({
-            name: 'Permohonan Telah Dikemaskini!',
-            iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236794583732457473/7828-verify-ak.gif?ex=66394e37&is=6637fcb7&hm=923d3f3b300606a2ae4ceb7bae980fd533a4c5ee2cf73111569a892a595f1f69&', 
-            url: 'https://discord.gg/X6RT5VdJPQ'
+          name: 'Permohonan Telah Dikemaskini!',
+          iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236794583732457473/7828-verify-ak.gif?ex=66394e37&is=6637fcb7&hm=923d3f3b300606a2ae4ceb7bae980fd533a4c5ee2cf73111569a892a595f1f69&',
+          url: 'https://discord.gg/X6RT5VdJPQ'
         })
         .setDescription('➡️ **Permohonan anda telah berjaya diproses.**\n➡️** Sila gunakan butang di bawah untuk mengawal radio ini.**'),
+      new EmbedBuilder()
+        .setColor('#ffea00')
+        .setAuthor({
+          name: 'Permohonan Telah Dikemaskini!',
+          iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802032938127470/4104-verify-yellow.gif?ex=66395527&is=663803a7&hm=71a7fba7f91897e52d9645b45d85d3da0ff97af2b63d10960004e68ff40d9c3b&',
+          url: 'https://discord.gg/X6RT5VdJPQ'
+        })
+        .setDescription('➡️ **Permohonan anda telah berjaya diproses.**\n➡️** Sila gunakan butang di bawah untuk mengawal radio ini.**'),
+      new EmbedBuilder()
+        .setColor('#FF0000')
+        .setAuthor({
+          name: 'Permohonan Telah Dikemaskini!',
+          iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802049190920202/4104-verify-red.gif?ex=6639552b&is=663803ab&hm=8dbc851fe56441b916a0044152dd517ed26434a11ee506518382f380c527c3bd&',
+          url: 'https://discord.gg/X6RT5VdJPQ'
+        })
+        .setDescription('➡️ **Permohonan anda telah berjaya diproses.**\n➡️** Sila gunakan butang di bawah untuk mengawal radio ini.**')
+    ];
 
- 
-    new EmbedBuilder()
-    .setColor('#ffea00')
-    .setAuthor({
-        name: 'Permohonan Telah Dikemaskini!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802032938127470/4104-verify-yellow.gif?ex=66395527&is=663803a7&hm=71a7fba7f91897e52d9645b45d85d3da0ff97af2b63d10960004e68ff40d9c3b&', 
-        url: 'https://discord.gg/X6RT5VdJPQ'
-    })
-    .setDescription('➡️ **Permohonan anda telah berjaya diproses.**\n➡️** Sila gunakan butang di bawah untuk mengawal radio ini.**'),
+    const randomIndex = Math.floor(Math.random() * embeds.length);
+    await interaction.followUp({ embeds: [embeds[randomIndex]] });
 
-  
-    new EmbedBuilder()
-    .setColor('#FF0000')
-    .setAuthor({
-        name: 'Permohonan Telah Dikemaskini!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802049190920202/4104-verify-red.gif?ex=6639552b&is=663803ab&hm=8dbc851fe56441b916a0044152dd517ed26434a11ee506518382f380c527c3bd&', 
-        url: 'https://discord.gg/X6RT5VdJPQ'
-    })
-    .setDescription('➡️ **Permohonan anda telah berjaya diproses.**\n➡️** Sila gunakan butang di bawah untuk mengawal radio ini.**')
-];
+  } catch (error) {
+    console.error('Error processing play command:', error);
+    const errorEmbed = new EmbedBuilder()
+      .setColor('#ff0000')
+      .setTitle('Error')
+      .setDescription('Terdapat ralat ketika memproses permohonan anda.');
 
+    await interaction.editReply({ embeds: [errorEmbed] });
+  }
+}
 
-const randomIndex = Math.floor(Math.random() * embeds.length);
-
-
-await interaction.followUp({ embeds: [embeds[randomIndex]] });
-
-    } catch (error) {
-        console.error('Error processing play command:', error);
-        const errorEmbed = new EmbedBuilder()
-            .setColor('#ff0000')
-            .setTitle('Error')
-            .setDescription('Terdapat ralat ketika memproses permohonan anda.');
-
-     
-        await interaction.editReply({ embeds: [errorEmbed] });
-    }
+async function autocomplete(client, interaction) {
+  const focusedOption = interaction.options.getFocused();
+  const filtered = queueNames.filter(name => name.toLowerCase().includes(focusedOption.toLowerCase()));
+  await interaction.respond(
+    filtered.map(choice => ({ name: choice, value: choice }))
+  );
 }
 
 module.exports = {
-    name: "play",
-    description: "Mainkan lagu",
-    permissions: "0x0000000000000800",
-    options: [{
-        name: 'nama',
-        description: 'Masukkkan nama lagu / link atau playlist',
-        type: ApplicationCommandOptionType.String,
-        required: true
-    }],
-    run: play,
-    queueNames: queueNames
+  name: "play",
+  description: "Mainkan lagu",
+  permissions: "0x0000000000000800",
+  options: [{
+    name: 'nama',
+    description: 'Masukkkan nama lagu / link atau playlist',
+    type: ApplicationCommandOptionType.String,
+    required: true,
+    autocomplete: true
+  }],
+  run: play,
+  autocomplete: autocomplete,
+  queueNames: queueNames
 };
