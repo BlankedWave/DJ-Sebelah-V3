4b6d05d001cf037ee0a747102ccc4e515e842981
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-06-03 13:02:36 +0800

    Update player.js

--- player.js
+++ player.js
@@ -1,23 +1,4 @@
-/*
 
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
 const { Riffy } = require("riffy");
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const { queueNames } = require("./commands/play"); 
@@ -59,41 +40,41 @@ function initializePlayer(client) {
         const embed = new EmbedBuilder()
         .setColor("#0099ff")
         .setAuthor({
-            name: 'Now Playing',
+            name: 'Sedang Dimainkan',
             iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236664581364125787/music-play.gif?ex=6638d524&is=663783a4&hm=5179f7d8fcd18edc1f7d0291bea486b1f9ce69f19df8a96303b75505e18baa3a&', 
-            url: 'https://discord.gg/xQF9f9yUEM'
+            url: 'https://discord.gg/X6RT5VdJPQ'
         })
-        .setDescription(`➡️ **Song Name:** [${track.info.title}](${track.info.uri})\n➡️ **Author:** ${track.info.author}\n➡️ **Platforms :** YouTube, Spotify, SoundCloud`)
+        .setDescription(`➡️ **Nama Lagu:** [${track.info.title}](${track.info.uri})\n➡️ **Penyanyi:** ${track.info.author}\n➡️ **Platform :** YouTube, Spotify, SoundCloud`)
 
         .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=663939fa&is=6637e87a&hm=e02431de164b901e07b55d8f8898ca5b1b2832ad11985cecc3aa229a7598d610&`)
         .setThumbnail(track.info.thumbnail)
         .setTimestamp()
-        .setFooter({ text: 'Click below buttons to control playback!'}); 
+        .setFooter({ text: 'Sila gunakan butang di bawah untuk mengawal radio ini!'}); 
 
 
 
         const queueLoopButton = new ButtonBuilder()
             .setCustomId("loopQueue")
-            .setLabel("Loop Queue")
+            .setLabel("Loop On 🟢")
             .setStyle(ButtonStyle.Primary);
 
         const disableLoopButton = new ButtonBuilder()
             .setCustomId("disableLoop")
-            .setLabel("Disable Loop")
+            .setLabel("Loop Off 🔴")
             .setStyle(ButtonStyle.Primary);
 
         const skipButton = new ButtonBuilder()
             .setCustomId("skipTrack")
-            .setLabel("Skip")
+            .setLabel("Langkau ⏩")
             .setStyle(ButtonStyle.Success);
 
         const showQueueButton = new ButtonBuilder()
             .setCustomId("showQueue")
-            .setLabel("Show Queue")
+            .setLabel("Queue 🎶")
             .setStyle(ButtonStyle.Primary);
         const clearQueueButton = new ButtonBuilder()
             .setCustomId("clearQueue")
-            .setLabel("Clear Queue")
+            .setLabel("Bersihkan 🗑️")
             .setStyle(ButtonStyle.Danger);
 
     
@@ -126,12 +107,12 @@ function initializePlayer(client) {
                 setLoop(player, 'queue');
                 const loopEmbed = new EmbedBuilder()
             .setAuthor({
-                    name: 'Queue Loop!',
+                    name: 'Loop Dihidupkan!',
                     iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157318080670728283/7905-repeat.gif?ex=66383bb4&is=6636ea34&hm=65f37cf88245f1c09285b547fda57b82828b3bbcda855e184f446d6ff43756b3&', 
-                    url: 'https://discord.gg/xQF9f9yUEM'
+                    url: 'https://discord.gg/X6RT5VdJPQ'
                 })
             .setColor("#00FF00")
-            .setTitle("**Queue loop is Activated!**")
+            .setTitle("**Queue loop dah dihidupkan!**")
          
 
         await channel.send({ embeds: [loopEmbed] });
@@ -140,11 +121,11 @@ function initializePlayer(client) {
                 const skipEmbed = new EmbedBuilder()
                 .setColor('#3498db')
                 .setAuthor({
-                  name: 'Song Skipped',
+                  name: 'Lagu Dilangkau',
                   iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&',
                   url: 'https://discord.gg/FUEHs7RCqz'
                 })
-            .setTitle("**Player will play the next song!**")
+            .setTitle("**Radio akan memainkan lagu yang seterusnya!**")
             .setTimestamp();
                
     
@@ -154,11 +135,11 @@ function initializePlayer(client) {
                 const loopEmbed = new EmbedBuilder()
                 .setColor("#0099ff")
                 .setAuthor({
-                    name: 'Looping Off',
+                    name: 'Loop Dimatikan',
                     iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif?ex=6638b97d&is=663767fd&hm=021725868cbbc66f35d2b980585489f93e9fd366aa57640732dc49e7da9a80ee&', 
-                    url: 'https://discord.gg/xQF9f9yUEM'
+                    url: 'https://discord.gg/X6RT5VdJPQ'
                 })
-                .setDescription('**Loop is Disabled for queue and single Song!**');
+                .setDescription('**Loop dah Dimatikan untuk queue dan sebuah Lagu!**');
                   
     
             await channel.send({ embeds: [loopEmbed] });
@@ -193,11 +174,11 @@ for (let i = 0; i < pages.length; i++) {
                 const queueEmbed = new EmbedBuilder()
                 .setColor("#0099ff")
                 .setAuthor({
-                    name: 'Queue Cleared',
+                    name: 'Queue Dibersihkan',
                     iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif?ex=6638b97d&is=663767fd&hm=021725868cbbc66f35d2b980585489f93e9fd366aa57640732dc49e7da9a80ee&', 
-                    url: 'https://discord.gg/xQF9f9yUEM'
+                    url: 'https://discord.gg/X6RT5VdJPQ'
                 })
-                .setDescription('**Queue Songs cleared sucessfully!**');
+                .setDescription('**Lagu-lagu di dalam queue berjaya dibersihkan!**');
                
     
             await channel.send({ embeds: [queueEmbed] });
@@ -219,7 +200,7 @@ for (let i = 0; i < pages.length; i++) {
             player.destroy();
             const queueEmbed = new EmbedBuilder()
                 .setColor("#0099ff")
-                .setDescription('**Queue Songs ended! Disconnecting Bot!**');
+                .setDescription('**Lagu di dalam queue tamat! Disconnecting Bot!**');
                
     
             await channel.send({ embeds: [queueEmbed] });
@@ -256,23 +237,4 @@ for (let i = 0; i < pages.length; i++) {
 
 module.exports = { initializePlayer };
 
-/*
 
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
