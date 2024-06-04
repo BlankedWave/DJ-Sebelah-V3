667a534294449588aeda7810a677001077ecb83f
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-06-04 14:02:01 +0800

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -4,21 +4,24 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "support",
-  description: "Dapatkan support server link",
+  description: "Mengenai bot dan discord link",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
 
       const supportServerLink = "https://discord.gg/X6RT5VdJPQ";
+      
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
-              name: 'Support Server',
+              name: 'DJ Sebelah',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/X6RT5VdJPQ'
           })
-            .setDescription(`➡️ **Sertai Discord server kami untuk support dan update:**\n- Discord - ${supportServerLink}\n\n`)
+            .setDescription(`👋🏻**Salam Sebelah Wahai Sahabat Organik Sekalian👋🏻**!\n
+            🕺🏻 **DJ Sebelah merupakan DJ yang telah dilantik oleh <@267958390389604362> untuk menyajikan lagu kegemaran anda di segenap server.** \n
+            ➡️ **Sertai Discord server kami untuk sebarang info dan update:**\n- Discord - ${supportServerLink}\n\n`)
             .setImage('https://media.tenor.com/WDwFEWQxdFAAAAAi/fish.gif')
             .setTimestamp();
 
