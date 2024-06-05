8469f812a6b41fe706759c0902be0a18f63ca5d5
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-06-05 12:08:18 +0800

    Update player.js

--- player.js
+++ player.js
@@ -6,10 +6,10 @@ const { queueNames } = require("./commands/play");
 function initializePlayer(client) {
     const nodes = [  //CHANGE NODES HERE!!
   {
-    host: "lavalink.oryzen.xyz",
-    port: 80, 
-    password: "oryzen.xyz", 
-     secure: false
+            host: "lava-v4.ajieblogs.eu.org",
+            password: "https://dsc.gg/ajidevserver",
+            port: 443,
+            secure: true
   },
     ];
 
