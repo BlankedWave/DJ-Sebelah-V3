44fdd828cc258a1b9a9b3f2384f50e7eaab14016
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-06-21 11:04:48 +0800

    Update player.js

--- player.js
+++ player.js
@@ -5,17 +5,18 @@ const { queueNames } = require("./commands/play");
 function initializePlayer(client) {
     const nodes = [
         {
-            /*
+            
             host: "lava-v4.ajieblogs.eu.org",
             port: 443,
             password: "https://dsc.gg/ajidevserver",
             secure: true
-            */
+            
+            /*
             host: "37.114.42.191",
             port: 7519,
             password: "danteisnttaken",
             secure: false
-            
+            */
         },
     ];
 
